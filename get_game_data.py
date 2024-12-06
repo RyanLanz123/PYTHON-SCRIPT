f844c362f7e1cfd9efe7376b8b5b2e0554a8f912
RyanLanz123 <ryanlanz6@gmail.com>
2024-12-06 12:35:44 +0000

    add path directory finder and creater

---
+++ get_game_data.py
@@ -0,0 +1,39 @@
+import os
+import json
+import shutil
+from subprocess import PIPE, run
+import sys
+
+GAME_DIR_PATTERN = "game"
+
+def find_all_game_paths(source):
+    game_paths = []
+
+    for root, dirs, files in os.walk(source):
+        for directory in dirs:
+            if GAME_DIR_PATTERN in directory.lower():
+                path = os.path.join(source, directory)
+                game_paths.append(path)
+        break
+    return game_paths
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+def main(source, target):
+    cwd = os.getcwd()
+    source_path = os.path.join(cwd, source)
+    target_path = os.path.join(cwd, target)
+
+    game_paths = find_all_game_paths(source_path)
+    
+    create_dir(target_path)
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) != 3:
+        raise Exception("You must pass a source and target directory - only.")
+    
+    source, target = args[1:]
+    main(source, target)
